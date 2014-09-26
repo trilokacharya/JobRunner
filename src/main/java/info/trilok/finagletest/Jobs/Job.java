08d2d35330e7354039e20adc2a2cf3443fd10e01
Trilok Acharya <tacharya@deerwalk.com>
2014-09-26 10:38:48 -0400

    Added start, end time and JobCommand fields to Job

--- src/main/java/info/trilok/finagletest/Jobs/Job.java
+++ src/main/java/info/trilok/finagletest/Jobs/Job.java
@@ -1,5 +1,9 @@
 package info.trilok.finagletest.Jobs;
 
+import info.trilok.finagletest.Jobs.Command.JobCommand;
+
+import java.util.Date;
+
 /**
  * Created by trilok on 8/25/2014.
  */
@@ -8,6 +12,9 @@ public abstract class Job {
 
     private Integer id;
     private JOB_STATUS status;
+    private Date startTime;
+    private Date endTime;
+    private JobCommand jobCommand;
 
     public Job(Integer id){this.id=id;}
 
@@ -28,5 +35,9 @@ public abstract class Job {
     public abstract String getOutput();
 
     public abstract void start();
+
+    public Date getStartTime(){return startTime;}
+    public Date getEndTime(){return endTime;}
+    public JobCommand getJobCommand(){return jobCommand;}
 
 }
