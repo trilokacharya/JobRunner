1943236806dcd9be659e70f056679a666aa7ddcd
Trilok Acharya <tacharya@deerwalk.com>
2014-10-01 12:59:27 -0400

    Added executorservice submission while also getting back error messages if any

--- src/main/java/info/trilok/finagletest/JobService.java
+++ src/main/java/info/trilok/finagletest/JobService.java
@@ -1,5 +1,7 @@
 package info.trilok.finagletest;
 
+import com.twitter.util.ExecutorServiceFuturePool;
+import com.twitter.util.Function0;
 import com.twitter.util.Future;
 import info.trilok.finagletest.Jobs.Factory.JobFactory;
 import info.trilok.finagletest.Jobs.Job;
@@ -9,26 +11,58 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Created by tacharya on 9/29/2014.
  */
 public class JobService {
     private static final int NUM_THREADS= 2; // won't be running many concurrent jobs
-    private HashMap<Long, Future<Job>> currentJobs = new HashMap<>();
+    private HashMap<Long, Job> currentJobs = new HashMap<>();
     private ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+    private ExecutorServiceFuturePool pool = new ExecutorServiceFuturePool(executorService);
+    String result="";
 
     public Future<String> runCommand(Request request){
         if(request.request == Request.REQUEST_TYPE.CREATE){
             try {
-                Job j=JobFactory.Create(request.details);
-                executorService.submit(j);
-                return Future.value(j.getId().toString());
+                final Job j=JobFactory.Create(request.details);
+                currentJobs.put(j.getId(),j);
+                j.setStatus(Job.JOB_STATUS.WAITING);
+                try {
+                    j.setupJob();
+                    result=j.getId().toString();
+                }catch(Exception ex){
+                    result="Error:"+ex.getMessage();
+                    System.err.println("Error:"+ex.getMessage());
+
+                }
+                executorService.submit(j); // actually run the job now
             }catch(IOException ioex){
                 System.err.println("Error creating job"+ioex.getMessage());
-                return Future.value("");
+            }
+        }else if(request.request == Request.REQUEST_TYPE.INFO){
+            Job j = currentJobs.get(request.jobId);
+            if(j==null){
+                result= "Job not found:"+request.jobId.toString();
+            }else{
+                switch(request.details.getCommand()){
+                    case GET_RUN_INFO:
+                        result = j.getJobInfo();
+                        break;
+                    case GET_STATUS:
+                        result = j.getStatus().toString();
+                        break;
+                    case GET_STDERR:
+                        result = j.getErrors();
+                        break;
+                    case GET_STDOUT:
+                        result = j.getOutput();
+                        break;
+                }
             }
         }
-        return Future.value("");
+
+        return Future.value(result);
     }
 }
