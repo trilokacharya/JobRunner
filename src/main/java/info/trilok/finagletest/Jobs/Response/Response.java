3f93a12f9c2097d73dfcf932b6f4fee1149a34de
Trilok Acharya <tacharya@deerwalk.com>
2014-10-03 14:39:40 -0400

    Added a response type

---
+++ src/main/java/info/trilok/finagletest/Jobs/Response/Response.java
@@ -0,0 +1,10 @@
+package info.trilok.finagletest.Jobs.Response;
+
+/**
+ * Created by tacharya on 10/3/2014.
+ */
+public class Response {
+    public enum RESPONSE_STATUS{OK,ERROR};
+    public String message;
+    public RESPONSE_STATUS status;
+}
